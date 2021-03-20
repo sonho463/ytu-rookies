e59c5b4f356b7d0293116f0459383ea9a9a9ea7d
sonho463 <son.ho463@gmail.com>
2021-03-20 10:49:28 +0900

    レイアウト微調整

--- scripts/main.js
+++ scripts/main.js
@@ -84,7 +84,7 @@ var mySwiper = new Swiper ('.swiper-container', {
 	// オプションパラメータ(一部のみ抜粋)
 	loop: true, // 最後のスライドまで到達した場合、最初に戻らずに続けてスライド可能にするか。
 	speed: 600, // スライドが切り替わるトランジション時間(ミリ秒)。
-	slidesPerView: 3, // 何枚のスライドを表示するか
+	slidesPerView: 2, // 何枚のスライドを表示するか
 	spaceBetween: 10, // スライド間の余白サイズ(ピクセル)
 	direction: 'horizontal', // スライド方向。 'horizontal'(水平) か 'vertical'(垂直)。effectオプションが 'slide' 以外は無効。
 	effect: 'slide', // "slide", "fade"(フェード), "cube"(キューブ回転), "coverflow"(カバーフロー) または "flip"(平面回転)
